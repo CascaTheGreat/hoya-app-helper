ae73089f7d42b03f99b14983a0542b442e2f8706
Leo Ledlow <jledlow22@gmail.com>
2025-12-13 19:05:33 -0500

    ts I swear when I find you

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,4 @@
 function App() {
-  const handleRedirect = () => {
-    // check if the user agent is an ios device
-    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
-
-    if (isIOS) {
-      // Redirect to the iOS App Store
-      window.location.href =
-        "https://apps.apple.com/us/app/the-hoya/id6755940250";
-    } else {
-      // Redirect to the Hoya website
-      window.location.href = "https://thehoya.com/";
-    }
-  };
-
-  //handleRedirect();
-
   return (
     <>
       <p> Please enable JavaScript.</p>
